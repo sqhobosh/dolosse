7e4910dc1f1b75dd9b73b0ffbaad1fcc1b193837
caseyc37 <caseyc37@gmail.com>
2020-04-17 18:58:51 +0200

    Adapted it to show no errors under pylint

--- dolosse/generic_json_consumer.py
+++ dolosse/generic_json_consumer.py
@@ -1,3 +1,4 @@
+"""Reads json data from a kafka topic and graphs it"""
 import json
 import time
 import sys
@@ -6,9 +7,9 @@ from matplotlib import pyplot
 import kafka
 
 
-
 def populate(variables, decode,
-             prepend=""):
+             x_axis_data, prepend=""):
+    """Takes the data from the dict and populates the variables"""
     for other_key in decode:
         valid = True
         name = prepend+other_key
@@ -19,6 +20,7 @@ def populate(variables, decode,
         if isinstance(decode[other_key], dict):
             populate(variables,
                      decode[other_key],
+                     x_axis_data,
                      name+": ")
         else:
             if name in variables:
@@ -38,87 +40,103 @@ def populate(variables, decode,
 
 
 
-topic = "default"
 
-screen = curses.initscr()
-curses.cbreak()
-screen.nodelay(True)
+def check():
+    """Checks that the right number of args are present"""
+    topic = "default"
+    if (len(sys.argv)) < 2:
+        print("Please provide a topic name on the command line!")
+        print("Using default topic name 'default'...")
+    else:
+        topic = sys.argv[1]
+    return topic
 
-if (len(sys.argv)) < 2:
-    print("Please provide a topic name on the command line!")
-    print("Using default topic name 'default'...")
-else:
-    topic = sys.argv[1]
 
-data = {}
-line = {}
-plots = {}
-figures = {}
-axes = {}
-x_axis_data = []
-count = 0
-pyplot.ion() #Interactive mode on
-#pyplot.plot([1, 2, 3, 4], [4, 1, 3, 2])
-# To consume latest messages and auto-commit offsets
-consumer = kafka.KafkaConsumer(topic,
-                               group_id='my-group',
-                               bootstrap_servers=['localhost:9092'],
-                               auto_offset_reset='earliest',
-                               enable_auto_commit=False)
-partition = kafka.TopicPartition(topic, 0)
+def redraw(figures, key):
+    """Redraw the given figure from a dict of figures"""
+    figures[key].canvas.draw()
+    figures[key].canvas.flush_events()
 
-exit_char = curses.ERR
 
-for message in consumer:
-    try:
-        NewData = json.loads(message.value)
-    except json.decoder.JSONDecodeError as problem:
-        print("Error: Flawed JSON in value read from kafka topic: ",
-              message.value)
-        print("Error message: ", problem)
-        NewData = ""
-    data.update(NewData)
-    print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                         message.offset, message.key,
-                                         data))
-    x_axis_data.append(count)
-    count += 1
 
-    populate(line, data)
 
-    for key in line:
-        while len(line[key]) < len(x_axis_data):
-            line[key].append(0) #Variable went missing from data.
-            #Buffer with zeroes for now.
-        print(key, line[key])
-        if key in plots:
-            axes[key][0].set_ydata(line[key])
-            axes[key][0].set_xdata(x_axis_data)
-            plots[key].relim()
-            plots[key].autoscale_view()
-            figures[key].canvas.draw()
-            figures[key].canvas.flush_events()
-        else:
-            figures[key], plots[key] = pyplot.subplots()
-            axes[key] = plots[key].plot(line[key])
-            plots[key].set_autoscaley_on(True)
-            plots[key].set_autoscalex_on(True)
-            plots[key].set_title(key)
-    while (consumer.end_offsets([partition])[partition]
-           == consumer.position(kafka.TopicPartition(topic, 0))):
+
+def main():
+    """Main function - reads json data from a kafka topic and graphs it"""
+
+    screen = curses.initscr()
+    curses.cbreak()
+    screen.nodelay(True)
+
+    topic = check()
+
+    data = {}
+    line = {}
+    plots = {}
+    figures = {}
+    axes = {}
+    x_axis_data = []
+    count = 0
+    pyplot.ion() #Interactive mode on
+    # To consume latest messages and auto-commit offsets
+    consumer = kafka.KafkaConsumer(topic,
+                                   group_id='my-group',
+                                   bootstrap_servers=['localhost:9092'],
+                                   auto_offset_reset='earliest',
+                                   enable_auto_commit=False)
+
+    exit_char = curses.ERR
+
+    for message in consumer:
+        try:
+            update = json.loads(message.value)
+        except json.decoder.JSONDecodeError as problem:
+            print("Error: Flawed JSON in value read from kafka topic: ",
+                  message.value)
+            print("Error message: ", problem)
+            update = ""
+        data.update(update)
+        print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                             message.offset, message.key,
+                                             data))
+        x_axis_data.append(count)
+        count += 1
+
+        populate(line, data, x_axis_data)
+
         for key in line:
-            figures[key].canvas.draw()
-            figures[key].canvas.flush_events()
-        time.sleep(0.2)
-        exit_char = screen.getch()
+            while len(line[key]) < len(x_axis_data):
+                line[key].append(0) #Variable went missing from data.
+                #Buffer with zeroes for now.
+            print(key, line[key])
+            if key in plots:
+                axes[key][0].set_ydata(line[key])
+                axes[key][0].set_xdata(x_axis_data)
+                plots[key].relim()
+                plots[key].autoscale_view()
+                redraw(figures, key)
+            else:
+                figures[key], plots[key] = pyplot.subplots()
+                axes[key] = plots[key].plot(line[key])
+                plots[key].set_autoscaley_on(True)
+                plots[key].set_autoscalex_on(True)
+                plots[key].set_title(key)
+        while (consumer.end_offsets([kafka.TopicPartition(topic, 0)])
+               [kafka.TopicPartition(topic, 0)]
+               == consumer.position(kafka.TopicPartition(topic, 0))):
+            for key in line:
+                redraw(figures, key)
+            time.sleep(0.2)
+            exit_char = screen.getch()
+            if exit_char != curses.ERR:
+                break
         if exit_char != curses.ERR:
             break
-    if exit_char != curses.ERR:
-        break
-    else:
         exit_char = screen.getch()
 
 
 
-curses.nocbreak()
-curses.endwin()
+    curses.nocbreak()
+    curses.endwin()
+
+main()
