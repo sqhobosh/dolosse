fea163d983dac1d05c1a3442d2ec12e7c6b62287
caseyc37 <caseyc37@gmail.com>
2020-04-20 14:51:48 +0200

    Main function now takes command-line arguments as parameters

--- dolosse/generic_json_consumer.py
+++ dolosse/generic_json_consumer.py
@@ -10,7 +10,7 @@ from matplotlib import pyplot
 
 
 def populate(variables, decode,
-             x_axis_data, prepend=""):
+             count, prepend=""):
     """Takes the data from the dict and populates the variables"""
     for other_key in decode:
         valid = True
@@ -22,7 +22,7 @@ def populate(variables, decode,
         if isinstance(decode[other_key], dict):
             populate(variables,
                      decode[other_key],
-                     x_axis_data,
+                     count,
                      name+": ")
         else:
             if name in variables:
@@ -32,7 +32,7 @@ def populate(variables, decode,
                     variables[name].append(
                         variables[name][-1])
             else:
-                variables[name] = [0]*(len(x_axis_data)-1)
+                variables[name] = [0]*(count-1)
                 if valid:
                     variables[name].append(following)
                 else:
@@ -40,7 +40,7 @@ def populate(variables, decode,
                     #Remember, the list might still be empty here
 
 
-def create():
+def get_args():
     """Creates and returns a kafka consumer"""
     parser = ArgumentParser()
     parser.add_argument("--topic", default="default",
@@ -52,21 +52,29 @@ def create():
     arguments = parser.parse_args()
 
     # To consume latest messages and auto-commit offsets
-    consumer = KafkaConsumer(arguments.topic,
-                             group_id=arguments.group,
-                             bootstrap_servers=[arguments.server],
-                             auto_offset_reset='earliest',
-                             enable_auto_commit=False)
-    return consumer
+    return arguments.topic, arguments.group, arguments.server
 
-def redraw(figures, key):
-    """Redraw the given figure from a dict of figures"""
-    figures[key].canvas.draw()
-    figures[key].canvas.flush_events()
+def redraw(figures):
+    """Redraw all figures from a dict of figures"""
+    for key in figures:
+        figures[key].canvas.draw()
+        figures[key].canvas.flush_events()
 
 
-def main():
-    """Main function - reads json data from a kafka topic and graphs it"""
+def update_data(data, value):
+    """Update the list of data, given some json input"""
+    try:
+        update = loads(value)
+    except decoder.JSONDecodeError as problem:
+        print("Error: Flawed JSON in value: ",
+              value)
+        print("Error message: ", problem)
+        update = ""
+    data.update(update)
+
+
+def graph(topic, group, server):
+    """Reads json data from a kafka topic and graphs it"""
 
     screen = initscr()
     cbreak()
@@ -81,48 +89,43 @@ def main():
     plots = {}
     figures = {}
     axes = {}
-    x_axis_data = []
     count = 0
     pyplot.ion() #Interactive mode on
     # To consume latest messages and auto-commit offsets
-    consumer = create()
+    consumer = KafkaConsumer(topic,
+                             group_id=group,
+                             bootstrap_servers=[server],
+                             auto_offset_reset='earliest',
+                             enable_auto_commit=False)
     keyboard_input = ERR
 
     for message in consumer:
-        try:
-            update = loads(message.value)
-        except decoder.JSONDecodeError as problem:
-            print("Error: Flawed JSON in value read from kafka topic: ",
-                  message.value)
-            print("Error message: ", problem)
-            update = ""
-        data.update(update)
-        x_axis_data.append(count)
+        update_data(data, message.value)
         count += 1
 
-        populate(line, data, x_axis_data)
+        populate(line, data, count)
 
         for key in line:
-            while len(line[key]) < len(x_axis_data):
+            while len(line[key]) < count:
                 line[key].append(0) #Variable went missing from data.
                 #Buffer with zeroes for now.
             if key in plots:
                 axes[key][0].set_ydata(line[key])
-                axes[key][0].set_xdata(x_axis_data)
+                axes[key][0].set_xdata(list(range(count)))
                 plots[key].relim()
                 plots[key].autoscale_view()
-                redraw(figures, key)
             else:
                 figures[key], plots[key] = pyplot.subplots()
                 axes[key] = plots[key].plot(line[key])
                 plots[key].set_autoscaley_on(True)
                 plots[key].set_autoscalex_on(True)
                 plots[key].set_title(key)
-        while (consumer.end_offsets([TopicPartition(message.topic, 0)])
-               [TopicPartition(message.topic, 0)]
-               == consumer.position(TopicPartition(message.topic, 0))):
-            for key in line:
-                redraw(figures, key)
+
+        redraw(figures)
+        while (consumer.end_offsets([TopicPartition(topic, 0)])
+               [TopicPartition(topic, 0)]
+               == consumer.position(TopicPartition(topic, 0))):
+            redraw(figures)
             sleep(0.2)
             keyboard_input = screen.getch()
             if keyboard_input != ERR:
@@ -135,5 +138,11 @@ def main():
     endwin()
 
 
+def main_args():
+    """Grabs parameters from the arguments"""
+    topic_arg, group_arg, server_arg = get_args()
+    graph(topic_arg, group_arg, server_arg)
+
+
 if __name__ == '__main__':
-    main()
+    main_args()
